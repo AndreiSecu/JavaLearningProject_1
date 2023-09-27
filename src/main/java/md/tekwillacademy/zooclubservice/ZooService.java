09578e2b0195041e6d0eabf8b1c84e3c370605a5
andsec <andrei.secu@snowsoftware.com>
2023-09-27 20:43:29 +0300

    Added the implementation of Zoo Service by using OOP

---
+++ src/main/java/md/tekwillacademy/zooclubservice/ZooService.java
@@ -0,0 +1,19 @@
+package md.tekwillacademy.zooclubservice;
+
+public class ZooService {
+
+    public static void main(String[] args) {
+        Animal charlyDog = new Dog("Charly", true);
+        System.out.println(charlyDog.getName());
+        charlyDog.eat();
+        charlyDog.makeSound();
+
+        Dog rexDog = new Dog("Rex");
+        System.out.println(rexDog.getName());
+
+        Cat bonitaCat = new Cat("Bonita");
+        bonitaCat.eat();
+        bonitaCat.makeSound();
+    }
+
+}
